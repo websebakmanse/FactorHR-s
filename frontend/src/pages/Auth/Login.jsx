c6da34d63253e608b0c93e2e10a27dab1081eb75
ithapesunil <ithapesunil25@gmail.com>
2026-05-24 23:52:08 +0530

    your message

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -81,7 +81,7 @@ const Login = () => {
           <a href="#" className="text-white hover:underline transition-all">
             Sign up
           </a>
-         <h1>welcome</h1>
+         <h1>welcome</h1><br></br>
         </p>
       </div>
     </div>
