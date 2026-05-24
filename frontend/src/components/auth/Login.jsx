6a37adfd14a0b06a45f849cb1f1de24c35e0271b
Sebak Mondal <sebakmajlishpur@gmail.com>
2026-05-24 14:35:16 +0530

    delete

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -81,9 +81,7 @@ const Login = () => {
           <a href="#" className="text-white hover:underline transition-all">
             Sign up
           </a>
-          <a href="#" className="text-white hover:underline transition-all">
-            Sign up
-          </a>
+         
         </p>
       </div>
     </div>
