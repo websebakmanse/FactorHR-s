87179f51da02dc73723051e405f5836f229b0ab3
Sebak Mondal <sebakmajlishpur@gmail.com>
2026-05-26 12:18:21 +0530

    auth provider

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthProvider = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default AuthProvider
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AuthProvider from './context/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  
   </StrictMode>,
 )
